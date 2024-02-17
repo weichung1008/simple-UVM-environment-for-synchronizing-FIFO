b7125a0ba12e91231243a5c3c1c9ad429924cf0a
weichung1008 <160299780+weichung1008@users.noreply.github.com>
2024-02-17 16:48:41 +0800

    Update testbench.sv
    Fix indent to 2

--- testbench.sv
+++ testbench.sv
@@ -16,51 +16,51 @@
 
 module tb_sync_fifo;
 
-    parameter WIDTH=16;
-	
-    bit wr_en, rd_en;
-    bit [WIDTH-1:0]data_in;
-    bit rst_n;
-    bit clk;
-    bit empty, full;
-    bit [WIDTH-1:0]data_out;
+	parameter WIDTH=16;
 
-    my_intf intf(clk, rst_n);
+	bit wr_en, rd_en;
+  bit [WIDTH-1:0]data_in;
+  bit rst_n;
+  bit clk;
+  bit empty, full;
+  bit [WIDTH-1:0]data_out;
+
+  my_intf intf(clk, rst_n);
+	
+	sync_fifo(
+		.wr_en     (intf.wr_en), 
+    .rd_en     (intf.rd_en), 
+    .data_in   (intf.data_in), 
+    .rst_n     (intf.rst_n), 
+    .clk       (intf.clk), 
+    .empty     (intf.empty),
+    .full      (intf.full),
+    .data_out  (intf.data_out)
+	);
   
-    sync_fifo(
-        .wr_en     (intf.wr_en), 
-        .rd_en     (intf.rd_en), 
-        .data_in   (intf.data_in), 
-        .rst_n     (intf.rst_n), 
-        .clk       (intf.clk), 
-        .empty     (intf.empty),
-        .full      (intf.full),
-        .data_out  (intf.data_out)
-      );
+  always #5 clk=~clk;
   
-    always #5 clk=~clk;
-  
-    initial begin
-        clk=1'b1;
-        rst_n=1'b0;
-        #25 rst_n=1'b1;
-    end
+  initial begin
+		clk=1'b1;
+    rst_n=1'b0;
+    #25 rst_n=1'b1;
+	end
     
-    initial begin
-        $display("%0t : uvm_config_db set block", $time);
-        uvm_config_db#(vif)::set(null, "", "drv_vif", intf);
-        uvm_config_db#(vif)::set(null, "", "mon_vif", intf);
-    end
+	initial begin
+		display("%0t : uvm_config_db set block", $time);
+    uvm_config_db#(vif)::set(null, "", "drv_vif", intf);
+    uvm_config_db#(vif)::set(null, "", "mon_vif", intf);
+	end
     
-    initial begin
-        $display("%0t : run_test block", $time); 
-        run_test();
-    end
+	initial begin
+		$display("%0t : run_test block", $time); 
+		run_test();
+	end
   
-    initial begin
-        $display("%0t : dump block", $time);
-        $dumpfile("dump.vcd");
-        $dumpvars;
-    end
+	initial begin
+		$display("%0t : dump block", $time);
+		$dumpfile("dump.vcd");
+		$dumpvars;
+	end
   
 endmodule
