e7366ffc7a6f23cba72464453367405b592aa2ac
weichung1008 <160299780+weichung1008@users.noreply.github.com>
2024-02-17 16:54:01 +0800

    Update testbench.sv
    remove redundant space

--- testbench.sv
+++ testbench.sv
@@ -16,9 +16,9 @@
 
 module tb_sync_fifo;
 
-	parameter WIDTH=16;
+  parameter WIDTH=16;
 
-	bit wr_en, rd_en;
+  bit wr_en, rd_en;
   bit [WIDTH-1:0]data_in;
   bit rst_n;
   bit clk;
@@ -27,8 +27,8 @@ module tb_sync_fifo;
 
   my_intf intf(clk, rst_n);
 	
-	sync_fifo(
-		.wr_en     (intf.wr_en), 
+  sync_fifo(
+    .wr_en     (intf.wr_en), 
     .rd_en     (intf.rd_en), 
     .data_in   (intf.data_in), 
     .rst_n     (intf.rst_n), 
@@ -36,31 +36,31 @@ module tb_sync_fifo;
     .empty     (intf.empty),
     .full      (intf.full),
     .data_out  (intf.data_out)
-	);
+  );
   
   always #5 clk=~clk;
   
   initial begin
-		clk=1'b1;
+    clk=1'b1;
     rst_n=1'b0;
     #25 rst_n=1'b1;
-	end
+  end
     
-	initial begin
-		display("%0t : uvm_config_db set block", $time);
+  initial begin
+    display("%0t : uvm_config_db set block", $time);
     uvm_config_db#(vif)::set(null, "", "drv_vif", intf);
     uvm_config_db#(vif)::set(null, "", "mon_vif", intf);
-	end
+  end
     
-	initial begin
-		$display("%0t : run_test block", $time); 
-		run_test();
-	end
+  initial begin
+    $display("%0t : run_test block", $time); 
+    run_test();
+  end
   
-	initial begin
-		$display("%0t : dump block", $time);
-		$dumpfile("dump.vcd");
-		$dumpvars;
-	end
+  initial begin
+    $display("%0t : dump block", $time);
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
   
 endmodule
