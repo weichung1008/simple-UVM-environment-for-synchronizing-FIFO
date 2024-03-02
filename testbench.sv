b0798967f855ad01b6bacaa5569d89e5bf1159c8
weichung1008 <160299780+weichung1008@users.noreply.github.com>
2024-03-02 22:48:16 +0800

    Update testbench.sv
    add callback files and test

--- testbench.sv
+++ testbench.sv
@@ -7,17 +7,20 @@
 `include "my_agent.sv"
 `include "my_scb.sv"
 `include "my_env.sv"
+`include "my_driver_callback.sv"
+`include "drv_print_cb.sv"
 `include "my_test.sv"
 `include "test_wr_only.sv"
 `include "test_rd_only.sv"
 `include "test_rd_wr.sv"
 `include "test_rd_wr_con.sv"
 `include "test_rd_wr_mix.sv"
+`include "test_rd_wr_with_cb.sv"
 
 module tb_sync_fifo;
 
   parameter WIDTH=16;
-
+	
   bit wr_en, rd_en;
   bit [WIDTH-1:0]data_in;
   bit rst_n;
@@ -26,7 +29,7 @@ module tb_sync_fifo;
   bit [WIDTH-1:0]data_out;
 
   my_intf intf(clk, rst_n);
-	
+  
   sync_fifo(
     .wr_en     (intf.wr_en), 
     .rd_en     (intf.rd_en), 
@@ -45,13 +48,13 @@ module tb_sync_fifo;
     rst_n=1'b0;
     #25 rst_n=1'b1;
   end
-    
+  
   initial begin
-    display("%0t : uvm_config_db set block", $time);
+    $display("%0t : uvm_config_db set block", $time);
     uvm_config_db#(vif)::set(null, "", "drv_vif", intf);
     uvm_config_db#(vif)::set(null, "", "mon_vif", intf);
   end
-    
+ 
   initial begin
     $display("%0t : run_test block", $time); 
     run_test();
@@ -60,7 +63,7 @@ module tb_sync_fifo;
   initial begin
     $display("%0t : dump block", $time);
     $dumpfile("dump.vcd");
-    $dumpvars;
+	$dumpvars;
   end
   
 endmodule
