fa267b0e62c30896e3c3fe1501f571fbd2c4fd18
weichungchen <160299780+weichung1008@users.noreply.github.com>
2024-03-20 23:01:44 +0800

    Update testbench.sv
    move the included files to uvm_env_pkg.sv

--- testbench.sv
+++ testbench.sv
@@ -1,22 +1,4 @@
-`include "my_intf.sv"
-`include "my_trans.sv"
-`include "my_sequencer.sv"
-`include "my_sequence.sv"
-`include "my_driver.sv"
-`include "my_monitor.sv"
-`include "my_agent.sv"
-`include "my_scb.sv"
-`include "my_env.sv"
-`include "my_driver_callback.sv"
-`include "drv_print_cb.sv"
-`include "my_test.sv"
-`include "test_wr_only.sv"
-`include "test_rd_only.sv"
-`include "test_rd_wr.sv"
-`include "test_rd_wr_con.sv"
-`include "test_rd_wr_mix.sv"
-`include "test_rd_wr_with_cb.sv"
-`include "test_rd_wr_with_DPI.sv"
+`include "uvm_env_pkg.sv"
 
 module tb_sync_fifo;
   
@@ -64,7 +46,7 @@ module tb_sync_fifo;
   initial begin
     $display("%0t : dump block", $time);
     $dumpfile("dump.vcd");
-	$dumpvars;
+		$dumpvars;
   end
   
 endmodule
