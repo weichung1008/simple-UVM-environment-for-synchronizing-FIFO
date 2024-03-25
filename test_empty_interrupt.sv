ca17666615e1e93ca0399f50ea3cb2a1e684daa2
weichungchen <160299780+weichung1008@users.noreply.github.com>
2024-03-25 15:04:38 +0800

    Update and rename test_empty.sv to test_empty_interrupt.sv
    add interrupt sequence into empty test case

--- test_empty_interrupt.sv
+++ test_empty_interrupt.sv
@@ -1,11 +1,12 @@
-class test_empty extends my_test;
+class test_empty_interrupt extends my_test;
   
-  `uvm_component_utils(test_empty)
+  `uvm_component_utils(test_empty_interrupt)
   
   wr_seq wseq;
   rd_seq rseq;
+  my_isr_seq isr_seq;
   
-  function new(string name="test_empty", uvm_component parent);
+  function new(string name="test_empty_interrupt", uvm_component parent);
     super.new(name, parent);
   endfunction
   
@@ -13,12 +14,16 @@ class test_empty extends my_test;
     super.build_phase(phase);
     wseq=wr_seq::type_id::create("wseq");
     rseq=rd_seq::type_id::create("rseq");
+    isr_seq=my_isr_seq::type_id::create("isr_seq");
     uvm_config_db#(int)::set(null, "", "wr_repeat_num", 5);
     uvm_config_db#(int)::set(null, "", "rd_repeat_num", 6);
   endfunction
   
   virtual task run_phase(uvm_phase phase);
     phase.raise_objection(this);
+    fork
+      isr_seq.start(env.mst_agt.seqr);
+    join_none
     fork
       wseq.start(env.mst_agt.seqr);
       rseq.start(env.mst_agt.seqr);
