7afdfd5416a7d937f756e6746202d725abb0ba45
weichungchen <160299780+weichung1008@users.noreply.github.com>
2024-03-25 15:26:39 +0800

    Update my_trans.sv
    add code related interrupt

--- my_trans.sv
+++ my_trans.sv
@@ -6,6 +6,7 @@ class my_trans extends uvm_sequence_item;
        bit [15:0]data_out;
        bit empty;
        bit full;
+       bit interrupt;
 
   `uvm_object_utils_begin(my_trans)
     `uvm_field_int(wr_en, UVM_ALL_ON)
@@ -14,11 +15,11 @@ class my_trans extends uvm_sequence_item;
     `uvm_field_int(data_out, UVM_ALL_ON)
     `uvm_field_int(empty, UVM_ALL_ON)
     `uvm_field_int(full, UVM_ALL_ON)
+    `uvm_field_int(interrupt, UVM_ALL_ON)
   `uvm_object_utils_end
   
   function new(string name="my_trans");
     super.new(name);
   endfunction
 
-  
 endclass
